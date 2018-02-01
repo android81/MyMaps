a824dfca84a2b71551212df4b516801cb40bf42e
Hank Tom <hanktom@gmail.com>
2018-02-02 01:35:41 +0800

    啟用MyLocation與GPS定位

--- app/src/main/java/com/tom/mymaps/MapsActivity.java
+++ app/src/main/java/com/tom/mymaps/MapsActivity.java
@@ -1,7 +1,15 @@
 package com.tom.mymaps;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,6 +21,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private static final int REQUEST_LOCATION = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,10 +46,63 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION);
+        } else {
+            setupMyLocation();
+        }
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @SuppressLint("MissingPermission")
+    private void setupMyLocation() {
+        mMap.setMyLocationEnabled(true);
+        mMap.setOnMyLocationButtonClickListener(
+                new GoogleMap.OnMyLocationButtonClickListener() {
+                    @Override
+                    public boolean onMyLocationButtonClick() {
+                        //透過位置服務，取得目前裝置所在
+                        gpsLocation();
+                        return false;
+                    }
+                });
+    }
+
+    @SuppressLint("MissingPermission")
+    private void gpsLocation() {
+        LocationManager locationManager=
+                (LocationManager) getSystemService(LOCATION_SERVICE);
+        String provider = "gps";
+        Location location = locationManager.getLastKnownLocation(provider);
+        if (location != null) {
+            Log.i("LOCATION", location.getLatitude()+"/"
+                    +location.getLongitude());
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                    new LatLng(location.getLatitude(),
+                            location.getLongitude())
+                    , 15));
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_LOCATION:
+                if (grantResults.length > 0
+                        && grantResults[0] ==
+                        PackageManager.PERMISSION_GRANTED) {
+                    //使用者允許權限
+                    setupMyLocation();
+                } else {
+                    //使用者拒絕授權, 停用MyLocation功能
+                }
+                break;
+        }
     }
 }

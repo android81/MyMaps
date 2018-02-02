de838cdb6e58818f5e6cdaaa1961d5ae3d7f8741
Hank Tom <hanktom@gmail.com>
2018-02-02 11:03:04 +0800

    實作地圖標示Marker

--- app/src/main/java/com/tom/mymaps/MapsActivity.java
+++ app/src/main/java/com/tom/mymaps/MapsActivity.java
@@ -2,6 +2,7 @@ package com.tom.mymaps;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
@@ -10,6 +11,8 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
@@ -20,7 +23,9 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -30,6 +35,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private GoogleMap mMap;
     private static final int REQUEST_LOCATION = 2;
     LocationRequest locationRequest;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,14 +65,59 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                     REQUEST_LOCATION);
         } else {
-            setupMyLocation();
-            createLocationRequest();
-            fuseLocationRequest();
+            //自定義InfoWindow
+            mMap.setInfoWindowAdapter(
+                    new GoogleMap.InfoWindowAdapter() {
+                        @Override
+                        public View getInfoWindow(Marker marker) {
+                            return null;
+                        }
+
+                        @Override
+                        public View getInfoContents(Marker marker) {
+                            View view = getLayoutInflater().inflate(R.layout.info_window, null);
+                            TextView title = view.findViewById(R.id.info_title);
+                            title.setText("Title:" + marker.getTitle());
+                            TextView snippet = view.findViewById(R.id.info_snippet);
+                            snippet.setText("說明:" + marker.getSnippet());
+                            return view;
+
+                        }
+                    });
+            mMap.setOnMarkerClickListener(
+                    new GoogleMap.OnMarkerClickListener() {
+                        @Override
+                        public boolean onMarkerClick(Marker marker) {
+                            //自行加入程式碼
+                            new AlertDialog.Builder(MapsActivity.this)
+                                    .setTitle(marker.getTitle())
+                                    .setMessage(marker.getSnippet())
+                                    .setPositiveButton("OK", null)
+                                    .show();
+                            return true;
+
+                        }
+                    });
+
+            //台北101的位置
+            LatLng taipei101 = new LatLng(25.033408, 121.564099);
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                    taipei101, 15));
+            Marker marker = mMap.addMarker(new MarkerOptions()
+                    .position(taipei101)
+                    .title("101")
+                    .snippet("這是台北101"));
+            marker.showInfoWindow();
+//                    .setIcon(BitmapDescriptorFactory
+//                            .fromResource(R.drawable.map_marker));
+            /*setupMyLocation();
+                            createLocationRequest();
+                            fuseLocationRequest();*/
         }
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
+        /*LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
     }
 
     @SuppressLint("MissingPermission")
@@ -83,20 +134,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     }
                 });
     }
-    private void createLocationRequest(){
+
+    private void createLocationRequest() {
         locationRequest = new LocationRequest();
         locationRequest.setInterval(5000);
-        locationRequest.setFastestInterval (2000);
+        locationRequest.setFastestInterval(2000);
         locationRequest.setPriority(
                 LocationRequest.PRIORITY_HIGH_ACCURACY);
     }
 
     @SuppressLint("MissingPermission")
-    private void fuseLocationRequest(){
+    private void fuseLocationRequest() {
         FusedLocationProviderClient client =
                 LocationServices.getFusedLocationProviderClient(this);
         client.requestLocationUpdates(locationRequest,
-                new LocationCallback(){
+                new LocationCallback() {
                     @Override
                     public void onLocationResult(LocationResult locationResult) {
                         Location location = locationResult.getLastLocation();
